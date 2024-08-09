fabb183210910c35f21a21e58df40d1e085ee51c
Maksym Peretiatko <maksym.peretiatko@lnu.edu.ua>
2024-08-09 03:05:16 +0300

    made form elements

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,23 @@
-  
+document.addEventListener('DOMContentLoaded', function() {
+    new SlimSelect({
+        select: '#selectElement',
+        placeholder: 'Select an option',
+        settings: {
+            showSearch: false,  
+            placeholderText: "Custom Placeholder Text",
+            placeholder: 'Select an option',
+          }
+    });
+});
+
+//textarea symbols amount
+const textareasArr = document.querySelectorAll("textarea")
+const symbolsAmountCell = document.querySelectorAll(".symbols-amount span")
+
+for (let i = 0; i < textareasArr.length; i++) {
+    textareasArr[i].addEventListener("input", function() {
+        var value = textareasArr[i].value
+        symbolsAmountCell[i].innerHTML = value.length
+    })
+}
+
