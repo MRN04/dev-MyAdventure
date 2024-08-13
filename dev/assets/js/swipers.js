520ff154f6560c69db6e6a2530fe01af81e77064
Maksym Peretiatko <maksym.peretiatko@lnu.edu.ua>
2024-08-14 01:40:12 +0300

    made mobile version of best offers block and footer

--- dev/assets/js/swipers.js
+++ dev/assets/js/swipers.js
@@ -1,23 +1,48 @@
 const swiperAdvantages = new Swiper('.about-us__swiper', {
-    spaceBetween: 44,
+    spaceBetween: 20,
     loop: true,
     simulateTouch: true,
-    slidesPerView: 1,
+    slidesPerView: 1.29,
+    centeredSlides: true,
+    initialSlide: 0,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
         enabled: true,
     },
+
     breakpoints: {
         1025: {
             spaceBetween: 0,
             loop: false,
             slidesPerView: 3,
+            centeredSlides: false,
             simulateTouch: false,
+            watchSlidesVisibility: false,
             pagination: {
                 enabled: false,
                 el: '.swiper-pagination',
             }
+        },
+
+        400: {
+            slidesPerView: 1.4,
+        },
+
+        475: {
+            slidesPerView: 1.5,
+        },
+
+        550: {
+            slidesPerView: 1.7,
+        },
+
+        700: {
+            slidesPerView: 2,
+        },
+
+        900: {
+            slidesPerView: 2.5,
         }
     }
   });
